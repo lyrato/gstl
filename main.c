97a020bc8520f9d64a99bdc9a67996e38b486f82
root <root@leon.(none)>
2016-10-06 12:19:07 +0800

    for seek

--- main.c
+++ main.c
@@ -8,22 +8,22 @@ static gboolean bus_call(GstBus * bus, GstMessage * msg, gpointer data)
     GMainLoop * loop = (GMainLoop *) data; //这个是主循环的指针，在接受EOS消息时退出循环
 
     switch (GST_MESSAGE_TYPE(msg)) {
-        case GST_MESSAGE_EOS:
-            g_print("End of stream\n");
-            g_main_loop_quit(loop);
-            break;
-        case GST_MESSAGE_ERROR: {
-            gchar * debug;
-            GError * error;
-            gst_message_parse_error(msg, &error, &debug);
-            g_free(debug);
-            g_printerr("ERROR:%s\n", error->message);
-            g_error_free(error);
-            g_main_loop_quit(loop);
-            break;
-        }
-        default:
-            break;
+    case GST_MESSAGE_EOS:
+        g_print("End of stream\n");
+        g_main_loop_quit(loop);
+        break;
+    case GST_MESSAGE_ERROR: {
+        gchar * debug;
+        GError * error;
+        gst_message_parse_error(msg, &error, &debug);
+        g_free(debug);
+        g_printerr("ERROR:%s\n", error->message);
+        g_error_free(error);
+        g_main_loop_quit(loop);
+        break;
+    }
+    default:
+        break;
     }
 
     return TRUE;
@@ -38,19 +38,33 @@ static gboolean handle_keyboard(GIOChannel * source, GIOCondition cond, GstEleme
     }
 
     switch (g_ascii_tolower(str[0])) {
-        case 'p': {
+    case 'p': {
+        printf("\n%s %d\n", __FUNCTION__, __LINE__);
+
+        if (cnt == 1) {
             printf("\n%s %d\n", __FUNCTION__, __LINE__);
-
-            if (cnt == 1) {
-                printf("\n%s %d\n", __FUNCTION__, __LINE__);
-                gst_element_set_state(play, GST_STATE_PAUSED);
-            } else {
-                printf("\n%s %d\n", __FUNCTION__, __LINE__);
-                gst_element_set_state(play, GST_STATE_PLAYING);
-            }
-
-            cnt = 1 - cnt;
+            gst_element_set_state(play, GST_STATE_PAUSED);
+        } else {
+            printf("\n%s %d\n", __FUNCTION__, __LINE__);
+            gst_element_set_state(play, GST_STATE_PLAYING);
         }
+
+        cnt = 1 - cnt;
+    }
+    case 's': {
+        printf("\n%s %d\n", __FUNCTION__, __LINE__);
+        GstEvent *event;
+
+        event = gst_event_new_seek (1.0,
+                                    GST_FORMAT_TIME,
+                                    GST_SEEK_FLAG_FLUSH,
+                                    GST_SEEK_TYPE_SET, 100 * GST_SECOND,
+                                    GST_SEEK_TYPE_NONE, 0* GST_SECOND);
+
+        gst_element_send_event (play, event);
+
+
+    }
     }
 
     return TRUE;
